db93c244fce99709151d1b472b28e34ec854124e
limeng <lm2343635@126.com>
2017-03-24 17:50:15 +0900

    Add a simple AI program.

--- java/ai/src/main/java/codecheck/App.java
+++ java/ai/src/main/java/codecheck/App.java
@@ -1,9 +1,20 @@
 package codecheck;
 
 public class App {
-	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			System.out.println(args[i]);
-		}
-	}
+    public static void main(String[] args) {
+        // Length of agrs must be large than 2.
+        if (args.length < 2) {
+            return;
+        }
+        // Get last word.
+        String last = args[0];
+        char lastCharacter = last.charAt(last.length() - 1);
+        for (int i = 1, l = args.length; i < l; i++) {
+            String word = args[i];
+            if (word.charAt(0) == lastCharacter) {
+                System.out.println(word);
+                return;
+            }
+        }
+    }
 }
